43ec077b7ba547ba3f4d3d997ada088b3b67e5bf
dreeseh <1859@holbertonschool.com>
2022-04-04 16:39:36 +0000

    changed return val to -1

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: a pointer to the root node of the tree to check
@@ -45,7 +46,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 
 	if (tree == NULL)
-		return (0);
+		return (-1);
 
 	left_height = binary_tree_height(tree->left);
 	left_height++;
