85a333c0c95542b2baf52e040d10e36456a9f0e2
dreeseh <1859@holbertonschool.com>
2023-03-09 18:21:54 +0000

    betty fixes

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_rotate_left - rotates nodes to the left
+ *
+ * @tree: original binary tree
+ * Return: rotated binary_tree_t*
+ */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *parent, *tparent;
