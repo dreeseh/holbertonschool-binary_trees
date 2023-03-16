a923b7b7eea47d2542bc9a085c57c9a0cda1ceff
dreeseh <1859@holbertonschool.com>
2023-03-16 04:18:57 +0000

    task 6

---
+++ 124-sorted_array_to_avl.c
@@ -0,0 +1,62 @@
+#include "binary_trees.h"
+
+/**
+ * initialize_tree - creates an AVL tree with recursion
+ *
+ * @node: pointer to the node
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of element in the array
+ * @leftRight: 1 to adding on the left, 2 to adding on the right
+ * Return: is void
+ */
+void initialize_tree(avl_t **node, int *array, size_t size, int leftRight)
+{
+	size_t middle;
+
+	if (size == 0)
+		return;
+
+	middle = (size / 2);
+	middle = (size % 2 == 0) ? middle - 1 : middle;
+
+	if (leftRight == 1)
+	{
+		(*node)->left = binary_tree_node(*node, array[middle]);
+		initialize_tree(&((*node)->left), array, middle, 1);
+		initialize_tree(&((*node)->left), array + middle + 1, (size - 1 - middle), 2);
+	}
+	else
+	{
+		(*node)->right = binary_tree_node(*node, array[middle]);
+		initialize_tree(&((*node)->right), array, middle, 1);
+		initialize_tree(&((*node)->right), array + middle + 1, (size - 1 - middle), 2);
+	}
+}
+
+/**
+ * sorted_array_to_avl - builds an AVL tree from an array
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of element in the array
+ * Return: pointer to the avl_t root
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	avl_t *root;
+	size_t middle;
+
+	root = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	middle = (size / 2);
+
+	middle = (size % 2 == 0) ? middle - 1 : middle;
+
+	root = binary_tree_node(root, array[middle]);
+
+	initialize_tree(&root, array, middle, 1);
+	initialize_tree(&root, array + middle + 1, (size - 1 - middle), 2);
+
+	return (root);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -67,5 +67,9 @@ bst_t *in_order_successor(bst_t *root);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_avl(const binary_tree_t *tree);
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
 
 #endif /* _BINARY_TREES_H_ */
